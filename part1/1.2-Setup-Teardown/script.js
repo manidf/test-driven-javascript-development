3f28abcf77abd920a8bc9e8e244fad1a4146305d
Mannuel Ferreira <mannuel@wwc.co.za>
2015-08-22 23:51:32 +0200

    test cases added

--- part1/1.2-Setup-Teardown/script.js
+++ part1/1.2-Setup-Teardown/script.js
@@ -30,10 +30,37 @@ function testCase(name, tests) {
             // so we don't count success until all methods have run successfully
             successfull++;
         } catch (e) {
-            output(test + " failed:" + e.message, "c00");
+            output(test + " failed:" + e.message, "#c00");
         }
     }
 
     var color = successfull == testCount ? "#0c0" : "#c00";
     output("<strong>" + testCount + " tests, " + (testCount = successfull) + " failures</strong>", color);
 }
+
+testCase("strftime test", {
+    setUp: function () {
+        this.date = new Date(2009, 9, 2, 22, 14, 45);
+    },
+    "test format specifier Y": function () {
+        assert("%Y should return full year", this.date.strftime("%Y") ==  2009);
+    },
+    "test format specifier %m": function () {
+        assert("%m should return month", Date.formats.m(this.date) === "10");
+    },
+    "test format specifier %d": function () {
+        assert("%d should return date", Date.formats.d(this.date) === "02");
+    },
+    "test format shorthand %F": function () {
+        assert("%F should be shortcut for %Y-%m-%d", Date.formats.F === "%Y-%m-%d");
+    }
+});
+
+Date.formats = {
+  j: function (date) {
+      var jan1 = new Date(date.getFullYear(), 0, 1);
+      var diff = date.getTime() - jan1.getTime();
+
+      return Math.ceil(diff / 864000000);
+  }
+};
